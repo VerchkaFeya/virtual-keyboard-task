b875870feb3d7aa6aaff107f763648026462d24c
Vera Krasnova <azirafail@mail.ru>
2022-05-08 12:54:09 +0300

    feat: add functions generating text

--- src/js/index.js
+++ src/js/index.js
@@ -106,3 +106,270 @@ if (lang === "en") {
 } else {
   generateKeys(keysDataRu, rows);
 }
+
+const textArea = document.querySelector("textarea");
+
+// функция ищет и меняет всо содержимое клавиш на большие буквы и значения с шифтом
+const changeToUpperCase = () => {
+  document.querySelectorAll(".key").forEach(item => {
+    if (!item.classList.contains("arrow-key") && !item.classList.contains("func-key") && item.dataset.shifted !== "null") {
+      let key = item;
+      key.innerHTML = key.dataset.shifted;
+    }
+  });
+};
+
+// функция ищет и  меняет все содержимое клавиш на маленькие буквы и значения без шифта
+const changeToLowerCase = () => {
+  document.querySelectorAll(".key").forEach(item => {
+    if (!item.classList.contains("arrow-key") && !item.classList.contains("func-key") && item.dataset.shifted !== "null") {
+      let key = item;
+      key.innerHTML = key.dataset.value;
+    }
+  });
+};
+
+// функция ищет и меняет содержимое ТОЛЬКО буквенных клавиш на большие
+const changeToUpperCaseCapsLock = () => {
+  document.querySelectorAll(".key").forEach(item => {
+    let code = item.dataset.code;
+    if (code.indexOf("Key") !== -1) {
+      let key = item;
+      key.innerHTML = key.innerHTML.toLocaleUpperCase();
+    }
+  });
+};
+
+// функция вставляет значение клавиши в место где стоит курсор (не касается backspace, del, arrows)
+const insertKeyValue = (node, insertValue) => {
+  let text = node.value;
+  let cursor = node.selectionStart;
+  let chunk1 = text.slice(0, cursor);
+  let chunk2 = text.slice(cursor);
+  chunk1 += insertValue;
+  text = chunk1 + chunk2;
+  node.value = text;
+  node.selectionStart = cursor + insertValue.length;
+  node.selectionEnd = cursor + insertValue.length;
+};
+
+const insertDeleteValue = (node) => {
+  let text = node.value;
+  let cursor = node.selectionStart;
+  let chunk1 = text.slice(0, cursor);
+  let chunk2 = text.slice(cursor + 1);
+  text = chunk1 + chunk2;
+  textArea.value = text;
+  textArea.selectionStart = cursor;
+  textArea.selectionEnd = cursor;
+};
+
+const insertBackspaceValue = (node) => {
+  let text = node.value;
+  let cursor = node.selectionStart;
+  let endCursor = node.selectionEnd;
+  if (cursor === endCursor) {
+    let chunk1 = text.slice(0, cursor - 1);
+    let chunk2 = text.slice(cursor);
+    text = chunk1 + chunk2;
+    node.value = text;
+    node.selectionStart = cursor - 1;
+    node.selectionEnd = cursor - 1;
+  }
+};
+
+const insertKeyValueOnClick = (e) => {
+  let key = e.target;
+  if (key.classList.contains("key")) {
+    textArea.focus();
+    if (key.dataset.code.indexOf("Key") !== -1) {
+      console.log("letter clicked!");
+      if (capsLock && !shift) {
+        insertKeyValue(textArea, `${key.dataset.shifted}`);
+      } else if (capsLock && shift) {
+        insertKeyValue(textArea, `${key.dataset.value}`);
+      } else if (!capsLock && shift) {
+        insertKeyValue(textArea, `${key.dataset.shifted}`);
+      } else if (!capsLock && !shift) {
+        insertKeyValue(textArea, `${key.dataset.value}`);
+      }
+    }
+    if (!functiionalKeys.includes(key.dataset.code) && !arrowKeys.includes(key.dataset.code) && key.dataset.code.indexOf("Key") === -1) {
+      console.log("double value keys clicked!");
+      if (capsLock && !shift) {
+        insertKeyValue(textArea, `${key.dataset.value}`);
+      } else if (capsLock && shift) {
+        insertKeyValue(textArea, `${key.dataset.shifted}`);
+      } else if (!capsLock && shift) {
+        insertKeyValue(textArea, `${key.dataset.shifted}`);
+      } else if (!capsLock && !shift) {
+        insertKeyValue(textArea, `${key.dataset.value}`);
+      }
+    }
+    if (arrowKeys.includes(key.dataset.code)) {
+      console.log("arrow cliked");
+      let cursor = textArea.selectionStart;
+      if (key.dataset.code === "ArrowLeft") {
+        textArea.selectionStart = cursor - 1;
+        textArea.selectionEnd = cursor - 1;
+      } else if (key.dataset.code === "ArrowRight") {
+        textArea.selectionStart = cursor + 1;
+        textArea.selectionEnd = cursor + 1;
+      } else if (key.dataset.code === "ArrowUp") {
+        insertKeyValue(textArea, "↑");
+      } else if (key.dataset.code === "ArrowDown") {
+        insertKeyValue(textArea, "↓");
+      }
+    }
+    if (key.dataset.code === "Tab") {
+      insertKeyValue(textArea, "    ");
+    }
+    if (key.dataset.code === "Delete") {
+      insertDeleteValue(textArea);
+    }
+    if (key.dataset.code === "Backspace") {
+      insertBackspaceValue(textArea);
+    }
+    if (key.dataset.code === "Space") {
+      insertKeyValue(textArea, " ");
+    }
+    if (key.dataset.code === "Enter") {
+      insertKeyValue(textArea, "\n");
+    }
+    if (key.dataset.code === "CapsLock") {
+      console.log("CAPS cliked");
+      if (capsLock) {
+        capsLock = false;
+        key.classList.remove("pressed");
+        changeToLowerCase();
+      } else {
+        capsLock = true;
+        key.classList.add("pressed");
+        changeToUpperCaseCapsLock();
+      }
+    }
+  }
+};
+
+// вставка символов при зажатии клавиш
+window.addEventListener("keydown", (e) => {
+  // console.log(`Key down: ${e.code} and ${e.key}`);
+  textArea.focus();
+  document.querySelector(`.key[data-code="${e.code}"]`).classList.add("pressed");
+  e.preventDefault();
+
+  if (e.code === "ShiftLeft" || e.code === "ShiftRight") {
+    shift = true;
+    changeToUpperCase();
+  }
+});
+
+// вставка символов при отжатии клавиш
+window.addEventListener("keyup", (e) => {
+  if (e.code !== "CapsLock") {
+    document.querySelector(`.key[data-code="${e.code}"]`).classList.remove("pressed");
+  }
+  if (e.code === "CapsLock" && capsLock === false) {
+    capsLock = true;
+    changeToUpperCaseCapsLock();
+  } else if (e.code === "CapsLock" && capsLock === true) {
+    capsLock = false;
+    changeToLowerCase();
+    document.querySelector(`.key[data-code="${e.code}"]`).classList.remove("pressed");
+  }
+  if (e.code === "ShiftLeft" || e.code === "ShiftRight") {
+    shift = false;
+    changeToLowerCase();
+  }
+
+  if (e.code.indexOf("Key") !== -1) {
+    let key = document.querySelector(`.key[data-code="${e.code}"]`);
+    if (capsLock && !shift) {
+      console.log("caps pressed and shift unpressed");
+      insertKeyValue(textArea, `${key.dataset.shifted}`);
+    } else if (capsLock && shift) {
+      insertKeyValue(textArea, `${key.dataset.value}`);
+      console.log("caps pressed and shift pressed");
+    } else if (!capsLock && shift) {
+      console.log("caps unpressed and shift pressed");
+      insertKeyValue(textArea, `${key.dataset.shifted}`);
+    } else if (!capsLock && !shift) {
+      console.log("caps unpressed and shift unpressed");
+      insertKeyValue(textArea, `${key.dataset.value}`);
+    }
+  }
+
+  if (!functiionalKeys.includes(e.code) && !arrowKeys.includes(e.code) && e.code.indexOf("Key") === -1) {
+    let key = document.querySelector(`.key[data-code="${e.code}"]`);
+    if (capsLock && !shift) {
+      console.log("caps pressed and shift unpressed");
+      insertKeyValue(textArea, `${key.dataset.value}`);
+    } else if (capsLock && shift) {
+      insertKeyValue(textArea, `${key.dataset.shifted}`);
+      console.log("caps pressed and shift pressed");
+    } else if (!capsLock && shift) {
+      console.log("caps unpressed and shift pressed");
+      insertKeyValue(textArea, `${key.dataset.shifted}`);
+    } else if (!capsLock && !shift) {
+      console.log("caps unpressed and shift unpressed");
+      insertKeyValue(textArea, `${key.dataset.value}`);
+    }
+  }
+
+  if (e.code === "Tab") {
+    insertKeyValue(textArea, "    ");
+  }
+  if (e.code === "Delete") {
+    insertDeleteValue(textArea);
+  }
+  if (e.code === "Backspace") {
+    insertBackspaceValue(textArea);
+  }
+  if (e.code === "Enter") {
+    insertKeyValue(textArea, "\n");
+  }
+  if (e.code === "Space") {
+    insertKeyValue(textArea, " ");
+  }
+  if (e.code.indexOf("Arrow") !== -1) {
+    let cursor = textArea.selectionStart;
+    if (e.code === "ArrowLeft") {
+      textArea.selectionStart = cursor - 1;
+      textArea.selectionEnd = cursor - 1;
+    } else if (e.code === "ArrowRight") {
+      textArea.selectionStart = cursor + 1;
+      textArea.selectionEnd = cursor + 1;
+    } else if (e.code === "ArrowUp") {
+      insertKeyValue(textArea, "↑");
+    } else if (e.code === "ArrowDown") {
+      insertKeyValue(textArea, "↓");
+    }
+  }
+});
+
+// window.addEventListener("keydown", (e) => {console.log(e.code); console.log(e.key)});
+window.addEventListener("click", insertKeyValueOnClick);
+
+// смена языка и генерация заново клавиатуры при зажатии клавиш шифт и альт
+document.addEventListener("keydown", function (event) {
+  if (event.shiftKey && event.altKey) {
+    console.log("CHANGE LANG");
+    console.log(lang);
+    if (lang === "en") {
+      lang = "ru";
+    } else {
+      lang = "en";
+    }
+    console.log(lang);
+    keyboardContainer.innerHTML = "";
+    generateRows(keyboardContainer);
+    rows = document.querySelectorAll(".keyboard__row");
+    if (lang === "ru") {
+      generateKeys(keysDataRu, rows);
+      document.querySelector(".text-field__change-lang").innerHTML = "Нажмите <span>Shift+Alt</span> для смены языка. \nТаск выполнен в ОС Windows";
+    } else {
+      generateKeys(keysDataEn, rows);
+      document.querySelector(".text-field__change-lang").innerHTML = "Press <span>Shift+Alt</span> to change language. \nTask created in Windows OS";
+    }
+  }
+});
